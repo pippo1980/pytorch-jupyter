379fffe436024f9a8a870bf05f680797d8e49e0f
pippo <duyaming@baidu.com>
2022-05-23 13:29:41 +0800

    add 1.0 for CIFAR10

--- 1.0.1/docker-run.sh
+++ 1.0.1/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -e JUPYTER_TOKEN=1q2w3e4r -it -p 18888:8888 --rm -v ~/data:/app/data:ro pippo1980/pytorch-jupyter:1.0.1
+docker run -d -e JUPYTER_TOKEN=1q2w3e4r -it -p 18888:8888 --rm -v ~/data:/home/jupyter/data:ro pippo1980/pytorch-jupyter:1.0.1
