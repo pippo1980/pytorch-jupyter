5a65c6bd983dea96201ab50a9dc5eba9dd04dc08
pippo <duyaming@baidu.com>
2022-05-25 21:38:34 +0800

    1.0.2 support inject jupyter token & base_project_url

--- 1.0.2/start.sh
+++ 1.0.2/start.sh
@@ -2,7 +2,8 @@
 
 
 # replace base_url
-export BASE_URL=${JUPYTER_BASE_URL-"jupyter"}
-sed -i 's/##JUPYTER_BASE_URL##/jupyter/' /home/jupyter/.jupyter/jupyter_config.template.py > jupyter_config.py
+export BASE_URL=${JUPYTER_BASE_URL:-"jupyter"}
+echo 'BASE_URL='$BASE_URL
+sed 's/##JUPYTER_BASE_URL##/'${BASE_URL}'/' /home/jupyter/.jupyter/jupyter_config.template.py > /home/jupyter/.jupyter/jupyter_config.py
 
 jupyter notebook --ip=0.0.0.0 --no-browser
