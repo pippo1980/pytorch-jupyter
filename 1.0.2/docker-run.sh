2f7fd411d5fcb072e443a57f1888a8869ef9f0c6
pippo <duyaming@baidu.com>
2022-05-25 21:46:56 +0800

    1.0.2 support inject jupyter token & base_project_url

--- 1.0.2/docker-run.sh
+++ 1.0.2/docker-run.sh
@@ -4,4 +4,4 @@ echo 'JUPYTER_PORT='${1:-'8888'}
 echo 'JUPYTER_TOKEN='${2:-'1q2w3e4r'}
 echo 'JUPYTER_BASE_URL='${3:-'jupyter'}
 
-docker run -e JUPYTER_PORT=${1:-'8888'} -e JUPYTER_TOKEN=${2:-'1q2w3e4r'} -e JUPYTER_BASE_URL=${3:-'jupyter'} -it -p ${1:-'8888'}:${1:-'8888'} --rm -v ~/data:/home/jupyter/data:ro pippo1980/pytorch-jupyter:1.0.2
+docker run -d -e JUPYTER_PORT=${1:-'8888'} -e JUPYTER_TOKEN=${2:-'1q2w3e4r'} -e JUPYTER_BASE_URL=${3:-'jupyter'} -it -p ${1:-'8888'}:${1:-'8888'} --rm -v ~/data:/home/jupyter/data:ro pippo1980/pytorch-jupyter:1.0.2
