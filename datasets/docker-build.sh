6ddad931f2f8272aeaf8bc59fe16f03934359714
pippo <duyaming@baidu.com>
2022-05-26 10:09:37 +0800

    add keras datasets volume

---
+++ datasets/docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t pippo1980/keras-datasets:1.0.0 .
---
+++ datasets/docker-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -it --name keras-datasets --rm -v /home/jupyter/data pippo1980/keras-datasets:1.0.0
