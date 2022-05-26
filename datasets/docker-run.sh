fcefc68615818d0ae5677980973e659eac34aa05
pippo <duyaming@baidu.com>
2022-05-26 11:27:22 +0800

    add keras datasets volume

--- datasets/docker-run.sh
+++ datasets/docker-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker run -d -it --name keras-datasets --rm -v /home/jupyter/data pippo1980/keras-datasets:1.0.0
+docker run -d -e JUPYTER_PORT=9999 -e JUPYTER_TOKEN=1q2w3e4r -e JUPYTER_BASE_URL=test -it -p 9999:9999 --rm --volumes-from keras-datasets pippo1980/pytorch-jupyter:1.0.2
